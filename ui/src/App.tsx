730b407f72251fcd0c6888ebcc7517bbd5f3f3d5
Dawid Piaskowski <dawid@MacBook-Pro.local>
2026-03-31 14:25:33 +0200

    Align first-run onboarding button label with e2e flow

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -238,7 +238,11 @@ function OnboardingRoutePage() {
                 : openOnboarding()
             }
           >
-            {matchedCompany ? "Add Agent" : "Start Onboarding"}
+            {matchedCompany
+              ? "Add Agent"
+              : companies.length > 0
+                ? "Start Onboarding"
+                : "New Company"}
           </Button>
         </div>
       </div>
