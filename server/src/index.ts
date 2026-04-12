69e93648f8f89d7be8ba44aeb5aad99069fdfd38
Chris Farhood <chris@farhood.org>
2026-04-12 09:07:31 -0400

    Reapply "fix(server): respect externally set PAPERCLIP_API_URL env var"
    This reverts commit 119bf6336ed350970559a4fa421f59156d494ab0.

--- server/src/index.ts
+++ server/src/index.ts
@@ -554,7 +554,9 @@ export async function startServer(): Promise<StartedServer> {
       : runtimeListenHost;
   process.env.PAPERCLIP_LISTEN_HOST = runtimeListenHost;
   process.env.PAPERCLIP_LISTEN_PORT = String(listenPort);
-  process.env.PAPERCLIP_API_URL = `http://${runtimeApiHost}:${listenPort}`;
+  if (!process.env.PAPERCLIP_API_URL) {
+    process.env.PAPERCLIP_API_URL = `http://${runtimeApiHost}:${listenPort}`;
+  }
   
   setupLiveEventsWebSocketServer(server, db as any, {
     deploymentMode: config.deploymentMode,
